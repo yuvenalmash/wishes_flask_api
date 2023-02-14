0cea3ed82160edcd02a246e6b4d102fa69be6cda
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-14 19:57:34 +0300

    Add function to create person

--- people.py
+++ people.py
@@ -1,27 +1,46 @@
 from datetime import datetime
-
+from flask import abort
 
 def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+  return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 
 PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    }
+  "Fairy": {
+    "fname": "Tooth",
+    "lname": "Fairy",
+    "timestamp": get_timestamp(),
+  },
+  "Ruprecht": {
+    "fname": "Knecht",
+    "lname": "Ruprecht",
+    "timestamp": get_timestamp(),
+  },
+  "Bunny": {
+    "fname": "Easter",
+    "lname": "Bunny",
+    "timestamp": get_timestamp(),
+  }
 }
 
+
 def read_all():
-    return list(PEOPLE.values())
+  return list(PEOPLE.values())
+
+
+def create(person):
+  lname = person.get("lname")
+  fname = person.get("fname", "")
+
+  if fname and lname not in PEOPLE:
+    PEOPLE[lname] = {
+      "lname": lname,
+      "fname": fname,
+      "timestamp": get_timestamp(),
+    }
+    return PEOPLE[lname], 201
+  else:
+    abort(
+      406,
+      f"Person with last name {lname} already exists",
+    )
