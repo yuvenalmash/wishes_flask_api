9349cfe62f9215e40233ee6e9e00aa4be76fb023
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-15 13:44:11 +0300

    Write data to db

--- people.py
+++ people.py
@@ -16,34 +16,36 @@ def read_one(lname):
 
 def create(person):
   lname = person.get("lname")
-  fname = person.get("fname", "")
+  existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-  if fname and lname not in PEOPLE:
-    PEOPLE[lname] = {
-      "lname": lname,
-      "fname": fname,
-      "timestamp": get_timestamp(),
-    }
-    return PEOPLE[lname], 201
+  if existing_person is None:
+    new_person = person_schema.load(person, session=db.session)
+    db.session.add(new_person)
+    db.session.commit()
+    return person_schema.dump(new_person), 201
+
   else:
-    abort(
-      406,
-      f"Person with last name {lname} already exists",
-    )
+    abort(406, f"Person with last name {lname} already exists")
 
 def update(lname, person):
-  if lname in PEOPLE:
-    PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
-    PEOPLE[lname]["timestamp"] = get_timestamp()
-    return PEOPLE[lname]
+  existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+  if existing_person:
+    update_person = person_schema.load(person, session=db.session)
+    existing_person.fname = update_person.fname
+    db.session.merge(existing_person)
+    db.session.commit()
+    return person_schema.dump(existing_person), 201
+
   else:
     abort(404, f"Person with last name {lname} not found")
 
 def delete(lname):
-  if lname in PEOPLE:
-    del PEOPLE[lname]
-    return make_response(
-      f"{lname} successfully deleted", 200
-    )
+  existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+  if existing_person:
+    db.session.delete(existing_person)
+    db.session.commit()
+    return make_response(f"{lname} successfully deleted", 200)
   else:
     abort(404, f"Person with last name {lname} not found")
